3a5394095ba0c73981239440830266f651b7e041
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-11-03 20:01:22 -0300

    Adiciona atividade Senha

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Olá usuário, por favor, digite sua senha: ");
+            string _senha = Console.ReadLine();
+
+            if(_senha == "1234abcd"){
+                Console.WriteLine("Acesso permitido.");
+            }
+            else{
+                Console.WriteLine("Acesso negado.");
+            }
+        }
+    }
+}
